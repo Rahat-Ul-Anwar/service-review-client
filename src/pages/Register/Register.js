0b1dda7527ee37d3a1a5c4906407009c277b758e
Rahat-Ul-Anwar <anwarrahat69@gmail.com>
2022-11-10 12:34:27 +0600

    register completed

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,11 +1,71 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import img from "../../assets/login.webp";
 
 const Register = () => {
-    return (
-        <div>
-            <h2>This is Register</h2>
+  return (
+    <div className="hero w-full my-20">
+      <div className="hero-content grid gap-20 md:grid-cols-2 flex-col lg:flex-row">
+        <div className="text-center lg:text-left">
+          <img className="w-3/4" src={img} alt="" srcset="" />
         </div>
-    );
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl py-20 bg-base-100">
+          <h1 className="text-4xl text-center font-bold">Sign Up</h1>
+          <form className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Your Name"
+                name="name"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="text"
+                placeholder="email"
+                name="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="text"
+                placeholder="password"
+                name="password"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control mt-6">
+              <input
+                className="btn btn-success "
+                type="submit"
+                value="Sign Up"
+              />
+            </div>
+          </form>
+          <p className="text-center">
+            Already have an account? please,
+            <Link className="text-cyan-500 font-semibold" to="/login">
+              Login
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
