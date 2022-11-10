263e10b698bf9daff07e12e64452a6b498617337
Rahat-Ul-Anwar <anwarrahat69@gmail.com>
2022-11-10 10:09:11 +0600

    setup folder structure

---
+++ src/Router/Routes/Routes.js
@@ -0,0 +1,29 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../layout/Main";
+import Blog from "../../pages/Blog/Blog";
+import Home from "../../pages/Home/Home/Home";
+import Login from "../../pages/Login/Login";
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+        ]
+}
+
+
+
+])
