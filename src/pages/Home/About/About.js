ba82b55899850b9528e883ee76316c6c94c89c03
Rahat-Ul-Anwar <anwarrahat69@gmail.com>
2022-11-10 11:08:01 +0600

    footer and about section done

--- src/pages/Home/About/About.js
+++ src/pages/Home/About/About.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
+import img from "../../../assets/Travelling-to-the-World.jpg";
 
 const About = () => {
-    return (
-        <div>
-            <h2>This is About</h2>
+  return (
+    <div className="hero min-h-screen bg-slate-300">
+      <div className="hero-content flex-col lg:flex-row">
+        <div className="w-2/3">
+          <img src={img} className=" rounded-lg shadow-2xl" alt="" />
         </div>
-    );
+        <div className="w-1/2">
+          <h1 className="text-5xl font-bold">Go outside!!</h1>
+          <p className="py-6">
+          A lot of people around the world travel every year to many places. Moreover, it is important to travel to humans
+          </p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import bannerImg from '../../../assets/about.jpg';
 
 const Banner = () => {
+
+  
     return (
-        <div>
-            <h2>This is Banner</h2>
+        <div className="hero pt-0">
+            <img src={bannerImg} alt="" />
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Do you want to travel</h1>
+            <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
         </div>
+      </div>
     );
 };
 
